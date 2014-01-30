993d9a2e7d1681cdadb07fe9701c43bcb60e0ee4
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2014-01-30 08:27:21 +0200

    new pattern code

--- OOPatterns/src/main/java/observer/WeatherData.java
+++ OOPatterns/src/main/java/observer/WeatherData.java
@@ -1,18 +1,75 @@
 package observer;
 
-import java.util.Observable;
-import java.util.Observer;
+// import java.util.Observable;
+// import java.util.Observer;
+import java.util.ArrayList;
 
 /**
  * Created by Lena on 28.01.14.
  */
-public class WeatherData extends Observable {
+//public class WeatherData extends Observable {  //uncomment if we use java.util.Observable
+public class WeatherData implements Subject {
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
     private float pressure;
-    public WeatherData() {
 
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
     }
 
     public float getPressure() {
         return pressure;
     }
+
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+
+    public WeatherData() {
+        observers = new ArrayList();
+    }
+
+    public void registerObservers(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o){
+        int i = observers.indexOf(o);
+        if (i >=0) {
+            observers.get(i);
+        }
+    }
+
+    public void notifyObservers(){
+        for (int i = 0; i< observers.size(); i++) {
+            Observer observer = (Observer) observers.get(i);
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        setTemperature(temperature);
+        setHumidity(humidity);
+        setPressure(pressure);
+        measurementsChanged();
+    }
+
+
 }
