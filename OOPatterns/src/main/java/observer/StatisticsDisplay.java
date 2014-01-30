43773a9efcc49d7b4e4f73773d459bc2a560c43e
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2014-01-30 08:47:27 +0200

    added code samples

---
+++ OOPatterns/src/main/java/observer/StatisticsDisplay.java
@@ -0,0 +1,38 @@
+package observer;
+
+import java.util.*;
+
+//use http://www.jetbrains.com/idea/webhelp/handling-lf-and-crlf-line-endings.html
+//to solve LF - CRLF problem
+public class StatisticsDisplay implements Observer, DisplayElement {
+    private float maxTemp = 0.0f;
+    private float minTemp = 200;
+    private float tempSum= 0.0f;
+    private int numReadings;
+    private WeatherData weatherData;
+
+    public StatisticsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temp, float humidity, float pressure) {
+        tempSum += temp;
+        numReadings++;
+
+        if (temp > maxTemp) {
+            maxTemp = temp;
+        }
+
+        if (temp < minTemp) {
+            minTemp = temp;
+        }
+
+        display();
+    }
+
+    public void display() {
+        System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+                + "/" + maxTemp + "/" + minTemp);
+    }
+}
